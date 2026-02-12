3d1b494f261299326d50e1c40785dbb0a4e75380
munoslat <munonour12@gmail.com>
2026-02-11 20:42:22 -0800

    add exercise-4.html

---
+++ exercise-4.js
@@ -0,0 +1,19 @@
+function add (price, quantity){
+    console.log(price + quantity)
+};
+
+add(20,80)
+add(10,30)
+add(7,15)
+add(12,10)
+add(31,10)
+
+
+const add2 = function(name) {
+    console.log("hello " + name)
+};
+
+add2("ahmed")
+add2("salmaan")
+add2("amiiro")
+add2("sahra")
