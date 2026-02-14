93ef0e88fb121b44f96ddf6a92a24086fa13ef81
munoslat <munonour12@gmail.com>
2026-02-13 21:18:14 -0800

    add exercise-7.html

---
+++ exercise-7.js
@@ -0,0 +1,23 @@
+let colors = [ "orange", "pink", "blue" ];
+
+console.log(colors);
+
+console.log("after push"),
+colors.push("black");
+console.log(colors);
+
+console.log("after pop"),
+colors.pop("");
+console.log(colors);
+
+console.log("after shift"),
+colors.shift("");
+console.log(colors);
+
+console.log("after unshift"),
+colors.unshift("orange");
+console.log(colors);
+
+
+console.log(colors.length);
+
