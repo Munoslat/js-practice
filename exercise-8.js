149e1684450ccdbd90accc8b7008ee975eaf2426
munoslat <munonour12@gmail.com>
2026-02-17 22:34:50 -0800

    changed tha hero text

--- exercise-8.js
+++ exercise-8.js
@@ -3,8 +3,13 @@ let car={
     model :"rav4",
     year : 2002,
 
+    start : function(){
+        console.log("the car has started", this.model);
+    }
 }
 console.log(car);
 console.log(car.make);
 console.log(car.model);
 console.log(car.year);
+
+console.log(car.start());
