c05ef247bffca461471401112737c3972390c6e7
munoslat <munonour12@gmail.com>
2026-02-19 02:21:54 -0800

    added exercise-10.html and exercise-10.js files

---
+++ exercise-10.js
@@ -0,0 +1,25 @@
+
+
+let library = [
+  
+  {
+     title: "things fall Apart",
+     Author:"chinua achebe",
+     year: 1958
+
+  },
+  {
+     title: "atomic habits",
+     Author:"james clear",
+     year: 1658
+
+  }
+
+
+
+];
+
+  console.log(library[0])
+console.log(library[1]);
+
+
