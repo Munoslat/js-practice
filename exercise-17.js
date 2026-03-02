3538edabf11bca441830e87118aa96d99705ae5a
munoslat <munonour12@gmail.com>
2026-03-01 23:05:18 -0800

    added exercise-17.html and exercise-17.js

---
+++ exercise-17.js
@@ -0,0 +1,14 @@
+
+let temperature=0;
+
+if(temperature < 0){
+console.log("very cold")
+
+}else if(temperature >= 0 && temperature <25){
+    console.log("cold")
+
+}else if(temperature >= 15 && temperature <25){
+console.log("warm")
+}else{
+    console.log("hot")
+}
