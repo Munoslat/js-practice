e6ab3d8d242c8e286b02aaf03ce23c04b0f0d8e0
munoslat <munonour12@gmail.com>
2026-03-05 20:38:49 -0800

    commit message: "Add default parameter to calculateAverage function"

---
+++ exercise24/default.js
@@ -0,0 +1,8 @@
+
+function calculateAverage (width, height=width) {
+
+   const area = width * height;
+   return area;
+ }
+   console.log(calculateAverage(5,4))
+   console.log(calculateAverage(6))
