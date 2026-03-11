2ada2e46801bc6136ec803a3057cd376a82e6c4c
munoslat <munonour12@gmail.com>
2026-03-10 20:08:15 -0700

    added exercise33

---
+++ exercise33/muno.js
@@ -0,0 +1,21 @@
+const header = document.querySelector("#header");
+
+console.log(header);
+
+const text = document.querySelector(".text")
+console.log(text)
+
+const button =document.querySelector("button")
+
+console.log(button)
+
+
+function changeText(){
+
+    header.textContent = "welcom to my websited";
+}
+
+function changeElement (){
+
+  text.innerHTML = "this is my first website";
+}
